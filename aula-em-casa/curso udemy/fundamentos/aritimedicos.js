05b03b0861083d643446ad4f3695f1859daf9e18
renanmcouto <66530084+renanmcouto@users.noreply.github.com>
2020-09-21 21:26:14 -0300

    Create aritimedicos.js

---
+++ aula-em-casa/curso udemy/fundamentos/aritimedicos.js
@@ -0,0 +1,9 @@
+const [a, b, c ,d] = [5, 4, 15, 3]
+
+const soma = a + b + c + d
+const subtracao = a - d
+const multiplicacao = c * b
+const divisao = c / a
+const modulo = b % d 
+
+console.log(soma, subtracao, multiplicacao, -divisao, modulo)
